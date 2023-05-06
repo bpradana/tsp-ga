791bf2b5041134ffe26d470115b1562de8a44c36
Bintang Pradana Erlangga Putra <bintang.pradana@privy.id>
2023-05-06 20:54:57 +0700

    wip: tsp ga

--- tsp.py
+++ tsp.py
@@ -1,71 +1,96 @@
-def non_dominated_sorting(solutions, objectives):
-    """
-    Perform non-dominated sorting on a list of solutions based on their objective scores.
+import random
 
-    Args:
-        solutions (list): A list of solutions.
-        objectives (list of lists): A list of lists containing the objective scores for each solution.
 
-    Returns:
-        A list of non-dominated fronts, where each front is a list of indices of the non-dominated solutions.
-    """
-    n = len(solutions)  # Number of solutions
-    ranks = [1] * n  # Initialize the ranks of all solutions to 1
-    dominance_counts = [0] * n  # Initialize the dominance count of all solutions to 0
-    fronts = [[]]  # Initialize the list of non-dominated fronts with an empty front
+# create a TSPGA class
+# this class will solve the TSP problem using a genetic algorithm
+class TSPGA:
+    def __init__(self, adjacency_matrix):
+        self.adjacency_matrix = adjacency_matrix
+        self.city_names = list(adjacency_matrix.keys())
 
-    # Calculate the dominance count of each solution
-    for i in range(n):
-        for j in range(n):
-            if all(
-                objectives[i][k] <= objectives[j][k]
-                for k in range(len(objectives[i]))
-                if i != j
-            ):
-                dominance_counts[i] += 1
+    def create_chromosome(self):
+        chromosome = random.sample(self.city_names, len(self.city_names))
+        return chromosome + [chromosome[0]]
 
-        # Add the solution to the first non-dominated front
-        if dominance_counts[i] == 0:
-            fronts[0].append(i)
+    def create_population(self, size):
+        return [self.create_chromosome() for _ in range(size)]
 
-    # Sort the solutions in each front by their crowding distance
-    for front in fronts:
-        distances = [0] * len(front)
-        for k in range(len(objectives)):
-            sorted_front = sorted(front, key=lambda x: objectives[x][k])
-            distances[0] = distances[-1] = float(
-                "inf"
-            )  # Set the boundary solutions' distance to infinity
-            for i in range(1, len(front) - 1):
-                distances[i] += (
-                    objectives[sorted_front[i + 1]][k]
-                    - objectives[sorted_front[i - 1]][k]
-                )
+    def fitness_function(self, chromosome):
+        fitness = 0
+        for i in range(len(chromosome) - 1):
+            fitness += self.adjacency_matrix[chromosome[i]][chromosome[i + 1]]
+        return fitness
 
-        # Sort the front by the crowding distance
-        front.sort(key=lambda x: distances[front.index(x)], reverse=True)
+    def fix_chromosome(self, chromosome):
+        chromosome = list(set(chromosome))
+        missing_cities = [city for city in self.city_names if city not in chromosome]
+        random.shuffle(missing_cities)
+        chromosome += missing_cities
+        chromosome += [chromosome[0]]
+        return chromosome
 
-        # Assign the same rank to solutions with the same crowding distance
-        for i in range(1, len(front)):
-            if all(
-                objectives[front[i]][k] == objectives[front[i - 1]][k]
-                for k in range(len(objectives[0]))
-            ):
-                ranks[front[i]] = ranks[front[i - 1]]
-            else:
-                ranks[front[i]] = ranks[front[i - 1]] + 1
+    def crossover(self, chromosome1, chromosome2, crossover_rate=0.7):
+        if random.random() > crossover_rate:
+            return chromosome1, chromosome2
+        start = random.randint(0, len(chromosome1) - 1)
+        end = random.randint(start, len(chromosome1) - 1)
+        new_chromosome1 = (
+            chromosome1[:start] + chromosome2[start:end] + chromosome1[end:]
+        )
+        new_chromosome2 = (
+            chromosome2[:start] + chromosome1[start:end] + chromosome2[end:]
+        )
+        return self.fix_chromosome(new_chromosome1), self.fix_chromosome(
+            new_chromosome2
+        )
 
-        # Add a new empty front for the next rank
-        fronts.append([])
+    def mutate(self, chromosome, mutation_rate=0.1):
+        if random.random() > mutation_rate:
+            return chromosome
+        start = random.randint(0, len(chromosome) - 1)
+        end = random.randint(start, len(chromosome) - 1)
+        new_chromosome = (
+            chromosome[:start] + chromosome[start:end][::-1] + chromosome[end:]
+        )
+        return self.fix_chromosome(new_chromosome)
 
-    # Create a list of non-dominated fronts
-    non_dominated_fronts = []
-    for rank in range(1, max(ranks) + 1):
-        non_dominated_front = [i for i in range(n) if ranks[i] == rank]
-        non_dominated_fronts.append(non_dominated_front)
+    def evaluate(self, population):
+        return sorted(population, key=self.fitness_function)
 
-    return non_dominated_fronts
+    def selection(self, population, elite_size=0.2):
+        return population[: int(len(population) * elite_size)]
+
+    def solve(self, population_size=100, generations=1000):
+        population = self.create_population(population_size)
+        for _ in range(generations):
+            population = self.evaluate(population)
+            selected = self.selection(population)
+            population = selected + [
+                self.mutate(self.crossover(*random.sample(selected, 2))[0])
+                for _ in range(population_size - len(selected))
+            ]
+        return population[0]
+
+
+def array_to_adjacency_matrix(array):
+    adjacency_matrix = {}
+    for i in range(len(array)):
+        adjacency_matrix[str(i)] = {}
+        for j in range(len(array)):
+            adjacency_matrix[str(i)][str(j)] = array[i][j]
+    return adjacency_matrix
 
 
 if __name__ == "__main__":
-    print("hello world")
+    adjacency_matrix = {
+        "A": {"A": 0, "B": 10, "C": 15, "D": 20},
+        "B": {"A": 10, "B": 0, "C": 35, "D": 25},
+        "C": {"A": 15, "B": 35, "C": 0, "D": 30},
+        "D": {"A": 20, "B": 25, "C": 30, "D": 0},
+    }
+
+    opt = TSPGA(adjacency_matrix)
+    waypoints = opt.solve()
+    cost = opt.fitness_function(waypoints)
+    print("Waypoints:", waypoints)
+    print("Cost:", cost)
